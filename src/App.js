7bdf490d85b20a91f078e259555061684a5ab11f
mmnngo <94153115+mmnngo@users.noreply.github.com>
2022-06-24 17:59:59 -0400

    Added bootstrap integration and some App.css styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Search from "./Search";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather Search Engine</h1>
-        <Search />
-      </header>
+      <div className="container">
+        <div className="card">
+          <h1>Weather Search Engine</h1>
+          <Search />
+        </div>
+      </div>
     </div>
   );
 }
