b373c2fe8d5058eda1a8a41385ea60bbdda186e4
mmnngo <94153115+mmnngo@users.noreply.github.com>
2022-06-24 21:03:21 -0400

    Added Netlify link

--- src/App.js
+++ src/App.js
@@ -81,6 +81,14 @@ function App() {
             rel="noopener noreferrer"
           >
             GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://chic-babka-35d9a7.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Netlify
           </a>
         </p>
       </div>
