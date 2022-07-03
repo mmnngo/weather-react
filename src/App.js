3df994bddf5fd446b93abda1bdcbd96eebc872c3
mmnngo <94153115+mmnngo@users.noreply.github.com>
2022-07-03 16:54:07 -0400

    created new Weather component to display current weather data, cleaned up App.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,78 +1,12 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Weather from "./Weather";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <div className="card">
-          <div className="Search">
-            <form id="search-form">
-              <div className="row">
-                <div className="col-6">
-                  <input
-                    type="search"
-                    autofocus="on"
-                    autocomplete="off"
-                    className="form-control"
-                    placeholder="Enter city here"
-                    id="enter-city-input"
-                  />
-                </div>
-                <div className="col-3">
-                  <input
-                    type="submit"
-                    value="Search"
-                    className="form-control btn btn-primary"
-                  />
-                </div>
-                <div className="col-3">
-                  <input
-                    type="submit"
-                    value="Current"
-                    className="form-control btn btn-outline-primary"
-                    id="current-location"
-                  />
-                </div>
-              </div>
-            </form>
-          </div>
-          <p className="current-date" id="date">
-            Friday, June 24, 2022 18:16
-          </p>
-          <div className="row">
-            <div className="col-8">
-              <h1>New York</h1>
-            </div>
-            <div className="col-4">
-              <span className="city-temperature" id="city-temperature">
-                25°C
-              </span>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-8">
-              <ul className="current-high-low">
-                <li id="weather-description">Rainy</li>
-                <li id="current-high-low"> 18°C / 6°C </li>
-                <li>Humidity: 30%</li>
-                <li>Wind: 8 km/hr</li>
-              </ul>
-            </div>
-            <div className="col-4">
-              <img
-                src="http://openweathermap.org/img/wn/10d@2x.png"
-                alt="clear"
-                id="icon"
-                className="icon"
-              />
-            </div>
-
-            <hr />
-            <div className="weather-forecast">Weather Forecast</div>
-          </div>
-        </div>
+        <Weather defaultCity="New York" />
         <p className="contact-me">
           This page was built by May Ngo and is open-sourced on{" "}
           <a
---
+++ src/Weather.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      city: response.data.name,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+
+      iconUrl: "http://openweathermap.org/img/wn/10d@2x.png",
+    });
+  }
+  if (weatherData.ready) {
+    return (
+      <div className="card">
+        <div className="Search">
+          <form id="search-form">
+            <div className="row">
+              <div className="col-6">
+                <input
+                  type="search"
+                  autofocus="on"
+                  autocomplete="off"
+                  className="form-control"
+                  placeholder="Enter city here"
+                  id="enter-city-input"
+                />
+              </div>
+              <div className="col-3">
+                <input
+                  type="submit"
+                  value="Search"
+                  className="form-control btn btn-primary"
+                />
+              </div>
+              <div className="col-3">
+                <input
+                  type="submit"
+                  value="Current"
+                  className="form-control btn btn-outline-primary"
+                  id="current-location"
+                />
+              </div>
+            </div>
+          </form>
+        </div>
+        <p className="current-date" id="date">
+          Friday, June 24, 2022 18:16
+        </p>
+        <div className="row">
+          <div className="col-8">
+            <h1>{weatherData.city}</h1>
+          </div>
+          <div className="col-4">
+            <span className="city-temperature" id="city-temperature">
+              {Math.round(weatherData.temperature)}°C
+            </span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-8">
+            <ul className="current-high-low">
+              <li id="weather-description">{weatherData.description}</li>
+              <li id="current-high-low"> 18°C / 6°C </li>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind} km/hr</li>
+            </ul>
+          </div>
+          <div className="col-4">
+            <img
+              src={weatherData.iconUrl}
+              alt={weatherData.description}
+              id="icon"
+              className="icon"
+            />
+          </div>
+
+          <hr />
+          <div className="weather-forecast">Weather Forecast</div>
+        </div>
+      </div>
+    );
+  } else {
+    const apiKey = "65b5662e22ff1bb3952c072792011a99";
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
+}
