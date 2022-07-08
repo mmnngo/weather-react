bc57ad4ee4af55e7d570d80cd75d6448280a9bde
mmnngo <94153115+mmnngo@users.noreply.github.com>
2022-07-08 00:56:05 -0400

    Updates

---
+++ precache-manifest.40d4b73f2b077dc56adcd30549671069.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0bfd99b013bc97c94a6f",
+    "url": "/myapp/static/css/main.9fe64f71.chunk.css"
+  },
+  {
+    "revision": "0bfd99b013bc97c94a6f",
+    "url": "/myapp/static/js/main.862b5fa7.chunk.js"
+  },
+  {
+    "revision": "6240cc95890da652388c",
+    "url": "/myapp/static/js/runtime~main.f19d3e6d.js"
+  },
+  {
+    "revision": "8a84f8dede56cbad84f3",
+    "url": "/myapp/static/css/2.3e5c57c3.chunk.css"
+  },
+  {
+    "revision": "8a84f8dede56cbad84f3",
+    "url": "/myapp/static/js/2.5c045fe8.chunk.js"
+  },
+  {
+    "revision": "bdee2a94363933c9465b",
+    "url": "/myapp/static/js/3.859f812d.chunk.js"
+  },
+  {
+    "revision": "f2d8517328f6751eb029c6f7ce06ec6f",
+    "url": "/myapp/index.html"
+  }
+];
